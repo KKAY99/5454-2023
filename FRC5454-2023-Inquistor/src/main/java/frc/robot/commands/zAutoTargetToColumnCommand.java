35a30499d866cfc4048909b0d02b6fe6f09a73f4
Karl Kay <kkay99@gmail.com>
2023-02-07 18:52:33 -0600

    Updated Column Target command

--- FRC5454-2023-Inquistor/src/main/java/frc/robot/commands/zAutoTargetToColumnCommand.java
+++ FRC5454-2023-Inquistor/src/main/java/frc/robot/commands/zAutoTargetToColumnCommand.java
@@ -10,7 +10,10 @@ public class zAutoTargetToColumnCommand extends CommandBase{
     private int m_pipeline;
     private double m_targetHeight;
     private double m_targetColumnDistance;
-    
+    private final MedianFilter m_filter = new MedianFilter(5);
+    private PIDController m_pidRight = new PIDController(Constants.PIDSteering.rightKP,PIDSteering.rightKI,PIDSteering.rightKD);
+    private PIDController m_pidLeft = new PIDController(Constants.PIDSteering.leftKP,PIDSteering.leftKI,PIDSteering.leftKD);
+        
     public zAutoTargetToColumnCommand(Limelight limelight,DrivetrainSubsystem drive,int gridChoice){
         m_limeLight = limelight;
         m_drive = drive;
@@ -61,68 +64,84 @@ public class zAutoTargetToColumnCommand extends CommandBase{
 
     @Override
     public void execute(){
-        m_limeLight.setPipeline(0);
+        m_limeLight.setPipeline(Constants.VisionPipelines.AprilTag);
     }
 
     @Override
     public boolean isFinished(){
         boolean returnValue = false;
-        boolean m_forward = false;
-        boolean m_back = false;
-        boolean m_right = false;
-        boolean m_left = false;
+        boolean forward = false;
+        boolean back = false;
+        boolean right = false;
+        boolean left = false;
+        int direction=0;
+        double speed=0;
+        int rotation=0; 
         
-        if(m_limeLight.getX()>m_targetColumnDistance){
-            m_right = true;
+        if(m_limeLight.getX()>Constants.LimeLightValues.kVisionXTolerance){
+            right = true;
 
         }else{
-            if (m_limeLight.getX()<m_targetColumnDistance){
-                m_left = true;
+            if (m_limeLight.getX()<-(0-Constants.LimeLightValues.kVisionXTolerance)){
+                left = true;
             }
         }
             
         if(m_limeLight.getArea()<Constants.ChargedUp.distanceFromTag){
-            m_forward = true;
+            forward = true;
         }else{
             if(m_limeLight.getArea()>Constants.ChargedUp.distanceFromTag){
-                m_back = true;
+                back = true;
             }
         }
+     //   double measurement = m_limeLight.getXRaw();
+     //   double filteredMeasurement = m_filter.calculate(measurement);
+     //   double pidOutput = m_pidRight.calculate(filteredMeasurement);
+     //   pidOutput=Math.min(Math.max(pidOutput,-0.10),.10);
+          
+        //determine direction, rotation and speed to move
         if(m_limeLight.isTargetAvailible()){
-            if(m_forward||m_back||m_right||m_left){
-                    if(m_left){
-                        if(m_forward){
-                            m_drive.movenodistance(45,0,0.1);
-    
-                        }else if(m_back){
-                            m_drive.movenodistance(135,0,0.1);
-    
+            if(forward||back||right||left){
+                    if(left){
+                        if(forward){
+                           direction=45;
+                           speed=0.1;
+                           
+                        }else if(back){
+                            direction=135;
+                            speed=0.1;
+                               
                         }else{
-                            m_drive.movenodistance(90,0,0.1);
+                           direction=90;
+                           speed=0.1;
+                           
     
                         }
                     }else{
-                        if(m_right){
-                            if(m_forward){
-                                m_drive.movenodistance(315,0,0.1);
-                
-                               }else if(m_back){
-                                m_drive.movenodistance(235,0,0.1);
-                
+                        if(right){
+                            if(forward){
+                               direction=315;
+                               speed=0.1;                                                
+                            }else if(back){
+                                direction=235;
+                                speed=0.1;                               
                                }else{
-                                m_drive.movenodistance(270,0,0.1);
-                
+                                direction=270;
+                                speed=0.1;                                               
                                }
         
                         }else{
-                            if(m_forward){
-                                m_drive.movenodistance(0,0,0.1);
+                            if(forward){
+                               direction=0;
+                               speed=0.1;                               
                             }else{
-                                m_drive.movenodistance(180,0,0.1);
+                                direction=180;
+                                speed=0.1;
                             }
                         }
         
                     }
+                    m_drive.movenodistance(direction, rotation, speed);
                     returnValue = false;
                 }else{
                     System.out.println("Is Not moving");
@@ -137,3 +156,4 @@ public class zAutoTargetToColumnCommand extends CommandBase{
         return returnValue;
     }
 }
+ 
