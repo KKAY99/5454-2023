72ddcd5c99f3e15edd344c16b0b7d15c5f47cc17
Jackson Y <kkay99@gmail.com>
2023-02-18 14:07:29 -0600

    removed clawSubsystem

--- FRC5454-2023-Inquistor/src/main/java/frc/robot/commands/zMoveArmRetract.java
+++ FRC5454-2023-Inquistor/src/main/java/frc/robot/commands/zMoveArmRetract.java
@@ -1,6 +1,6 @@
 package frc.robot.commands;
 import frc.robot.Constants;
-import frc.robot.subsystems.ClawSubsystem;
+
 import frc.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.RotateArmSubsystem;
